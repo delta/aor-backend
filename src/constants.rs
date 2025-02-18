6edbdbbfc48210702bb39c783ab295e409b6d085
Shriram Umapathy <rocketfuelhydrazine@gmail.com>
2025-02-18 14:06:02 +0530

    feat: game balancer

--- src/constants.rs
+++ src/constants.rs
@@ -12,10 +12,11 @@ pub const ATTACK_TOKEN_AGE_IN_MINUTES: i64 = 5;
 pub const GAME_AGE_IN_MINUTES: usize = 3;
 pub const MATCH_MAKING_ATTEMPTS: i32 = 10;
 pub const PERCENTANGE_ARTIFACTS_OBTAINABLE: f32 = 0.3;
-pub const BOMB_DAMAGE_MULTIPLIER: f32 = 5.0;
+pub const BOMB_DAMAGE_MULTIPLIER: f32 = 1.0;
 pub const COMPANION_BOT_RANGE: i32 = 5;
 pub const MOD_USER_BASE_PATH: &str = "storage/mod_user_base.json";
 pub const MAX_CHALLENGE_ATTEMPTS: i32 = 2;
+// pub const BOMB_DAMAGE_MULTIPLIER_FOR_DEFENDER: f32 = 0.5;
 
 pub struct HutLevelAttribute {
     pub defenders_limit: i32,
